789dcb5efa18958d83681fd9566be54c04ebcb00
jason <jason@LAPTOP-L90IPECE>
2017-11-14 10:38:25 -0500

    Init

---
+++ src/eecs2011a3/A3.java
@@ -0,0 +1,18 @@
+package eecs2011a3;
+
+public class A3 {
+	public A3() {
+		; // intentionally blank
+	}
+
+	public void run() {
+		// code goes here 
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A3 a3 = new A3();
+		a3.run();
+	}
+
+}
